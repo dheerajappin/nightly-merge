e19e37d72955a50c25b804197928989c12af86de
dheeraj <dheeraj.pant@appinventiv.com>
2022-04-22 12:19:52 +0530

    first

---
+++ entrypoint.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+set -e
+
+echo
+echo "  'Nightly Merge Action' is using the following input:"
+echo "    - stable_branch = '$INPUT_STABLE_BRANCH'"
+echo "    - development_branch = '$INPUT_DEVELOPMENT_BRANCH'"
+echo "    - allow_ff = $INPUT_ALLOW_FF"
+echo "    - allow_git_lfs = $INPUT_GIT_LFS"
+echo "    - ff_only = $INPUT_FF_ONLY"
+echo "    - allow_forks = $INPUT_ALLOW_FORKS"
+echo "    - user_name = $INPUT_USER_NAME"
+echo "    - user_email = $INPUT_USER_EMAIL"
+echo "    - push_token = $INPUT_PUSH_TOKEN = ${!INPUT_PUSH_TOKEN}"
+echo
+
+if [[ $INPUT_ALLOW_FORKS != "true" ]]; then
+  URI=https://api.github.com
+  API_HEADER="Accept: application/vnd.github.v3+json"
+  pr_resp=$(curl -X GET -s -H "${API_HEADER}" "${URI}/repos/$GITHUB_REPOSITORY")
+  if [[ "$(echo "$pr_resp" | jq -r .fork)" != "false" ]]; then
+    echo "Nightly merge action is disabled for forks (use the 'allow_forks' option to enable it)."
+    exit 0
+  fi
+fi
+
+if [[ -z "${!INPUT_PUSH_TOKEN}" ]]; then
+  echo "Set the ${INPUT_PUSH_TOKEN} env variable."
+  exit 1
+fi
+
+FF_MODE="--no-ff"
+if [[ "$INPUT_ALLOW_FF" == "true" ]]; then
+  FF_MODE="--ff"
+  if [[ "$INPUT_FF_ONLY" == "true" ]]; then
+    FF_MODE="--ff-only"
+  fi
+fi
+
+# Change based from:
+# * https://github.com/robotology/gh-action-nightly-merge/compare/master...tModLoader:master#
+# * https://github.com/actions/checkout/issues/766
+# * https://github.com/robotology/gh-action-nightly-merge/issues/11#issuecomment-1100451815
+# * (docs) https://git-scm.com/docs/git-config
+# * (GITHUB_WORKSPACE) https://docs.github.com/en/actions/learn-github-actions/environment-variables
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
+
+git remote set-url origin https://x-access-token:${!INPUT_PUSH_TOKEN}@github.com/$GITHUB_REPOSITORY.git
+git config --global user.name "$INPUT_USER_NAME"
+git config --global user.email "$INPUT_USER_EMAIL"
+
+set -o xtrace
+
+git fetch origin $INPUT_STABLE_BRANCH
+(git checkout $INPUT_STABLE_BRANCH && git pull)||git checkout -b $INPUT_STABLE_BRANCH origin/$INPUT_STABLE_BRANCH
+
+git fetch origin $INPUT_DEVELOPMENT_BRANCH
+(git checkout $INPUT_DEVELOPMENT_BRANCH && git pull)||git checkout -b $INPUT_DEVELOPMENT_BRANCH origin/$INPUT_DEVELOPMENT_BRANCH
+
+if git merge-base --is-ancestor $INPUT_STABLE_BRANCH $INPUT_DEVELOPMENT_BRANCH; then
+  echo "No merge is necessary"
+  exit 0
+fi;
+
+set +o xtrace
+echo
+echo "  'Nightly Merge Action' is trying to merge the '$INPUT_STABLE_BRANCH' branch ($(git log -1 --pretty=%H $INPUT_STABLE_BRANCH))"
+echo "  into the '$INPUT_DEVELOPMENT_BRANCH' branch ($(git log -1 --pretty=%H $INPUT_DEVELOPMENT_BRANCH))"
+echo
+set -o xtrace
+
+# Do the merge
+git merge $FF_MODE --no-edit $INPUT_STABLE_BRANCH
+
+# Pull lfs if enabled
+if [[ $INPUT_GIT_LFS == "true" ]]; then
+  git lfs pull
+fi
+
+# Push the branch
+git push origin $INPUT_DEVELOPMENT_BRANCH
